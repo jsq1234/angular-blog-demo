8655983eee743fb095761eacb24b3cf0aa4d7c05
jsq1234 <mananprakash3078@gmail.com>
2024-04-17 10:25:12 +0530

    added initial things

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  {
+    path: 'home',
+    pathMatch: 'full',
+    component: HomeComponent,
+    runGuardsAndResolvers: 'always',
+  },
+];
